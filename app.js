0b83d790d34abe46228f3f3521289b8ac8b9fb4a
eutagocode <thiagoluix21@gmail.com>
2024-06-01 04:33:30 -0300

    feat: :sparkles: Foi criada a função de comprimir uma imagem

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const sharp = require("sharp");
+const compressImage = require("compress-images");
+const fs = require("fs");
 
 const path = process.argv[2];
 const width = parseFloat(process.argv[3]);
@@ -7,10 +9,36 @@ const resize = (path, width) => {
     sharp(path)
         .resize({ width: width })
         .toFile("./temp/output_resize.jpg", (error) => {
+            console.log("Imagem redimensionada com sucesso!");
             if (error) throw error;
 
-            console.log("Imagem redimensionada com sucesso!");
+            compress("./temp/output_resize.jpg", "./compressed/");
         });
+};
+
+const compress = (pathInput, pathOutput) => {
+    compressImage(
+        pathInput,
+        pathOutput,
+        { compress_force: false, statistic: true, autoupdate: true },
+        false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        {
+            gif: {
+                engine: "gifsicle",
+                command: ["--colors", "64", "--use-col=web"],
+            },
+        },
+        (error, completed, statistic) => {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+        },
+    );
 };
 
 resize(path, width);
